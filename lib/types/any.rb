4fad439f59a6fcf2c18a7ab47a84905a132c0853
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-07-15 16:41:22 +1200

    Modernize code.

--- lib/types/any.rb
+++ lib/types/any.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2022-2024, by Samuel Williams.
+# Copyright, 2022-2025, by Samuel Williams.
 
 module Types
 	# Represents a union of multiple types. The first type to match the input is used.
